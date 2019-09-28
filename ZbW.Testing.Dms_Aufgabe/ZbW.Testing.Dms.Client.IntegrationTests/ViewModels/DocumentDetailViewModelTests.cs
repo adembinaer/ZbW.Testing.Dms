48bb6c2f5bddd4c9e166e9574fe7aa55173578b7
Adem <a.jusic86@gmail.com>
2019-09-28 14:57:52 +0200

    Commit vor finish

---
+++ ZbW.Testing.Dms_Aufgabe/ZbW.Testing.Dms.Client.IntegrationTests/ViewModels/DocumentDetailViewModelTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using ZbW.Testing.Dms.Client.ViewModels;
+
+namespace ZbW.Testing.Dms.Client.IntegrationTests.ViewModels {
+
+	[TestFixture]
+	internal class DocumentDetailViewModelTests {
+
+		// TODO CanSearch_CanReadFileName_HasFile
+		// TODO CanSave_HasAllRequiredFieldsSet_IsEnabled
+		// TODO CanSave_HasAllRequiredFieldsSet_IsDisabled
+		// TODO CanSave_HasSelectedDocument_IsEnabled
+		// TODO CanSave_HasSelectedDocument_IsDisbled
+		// TODO HasSaved_HasDocumentSaved_HasSaved
+		// TODO HasSaved_HasDocumentSaved_HasNotSaved
+		// TODO HasSaved_HasMetadataSaved_HasSaved
+		// TODO HasSaved_HasMetadataSaved_HasNotSaved
+
+	}
+}
--- ZbW.Testing.Dms_Aufgabe/ZbW.Testing.Dms.Client/Services/FileService.cs
+++ ZbW.Testing.Dms_Aufgabe/ZbW.Testing.Dms.Client/Services/FileService.cs
@@ -5,45 +5,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ZbW.Testing.Dms.Client.Services {
-	internal class FileService {
+namespace ZbW.Testing.Dms.Client.Services
+{
+    internal class FileService
+    {
 
-		// Property ist von aussen nicht lesbar
-		internal FileTestable FileTestable { private get; set; }
+        // Property ist von aussen nicht lesbar
+        internal FileTestable FileTestable { private get; set; }
 
-		public void CreateValutaFolderIfNotExists(string path) {
-			Directory.CreateDirectory(path);
-		}
+        public void CreateValutaFolderIfNotExists(string path)
+        {
+            Directory.CreateDirectory(path);
+        }
 
-		// Ein Standard für Property Injection wird im Konstruktor gesetzt
-		public FileService()
-		{
-			FileTestable = new FileTestable();
-		}
+        // Ein Standard für Property Injection wird im Konstruktor gesetzt
+        public FileService()
+        {
+            FileTestable = new FileTestable();
+        }
 
-		public void RemoveDocumentOnSource(string path) {
-			File.Delete(path);
-		}
+        public void RemoveDocumentOnSource(string path)
+        {
+            File.Delete(path);
+        }
 
-		public void CopyDocumentToTarge(String sourcePath, String targetPath) {
-			try
-			{
-				FileTestable.Copy(sourcePath, targetPath, true);
-			}
-			catch (Exception e)
-			{
-				throw new CouldNotCopyFileException("File konnte nicht kopiert werden", e);
-			}
-		}
+        public void CopyDocumentToTarge(String sourcePath, String targetPath)
+        {
+            try
+            {
+                FileTestable.Copy(sourcePath, targetPath, true);
+            }
+            catch (Exception e)
+            {
+                throw new CouldNotCopyFileException("File konnte nicht kopiert werden", e);
+            }
+        }
 
-		public String GetNewFileName(String typeName, String fileName, Guid guid) {
-			var fileExtension = this.GetFileExtension(fileName);
-			return $"{guid}_{typeName}.{fileExtension}";
-		}
+        public String GetNewFileName(String typeName, String fileName, Guid guid)
+        {
+            var fileExtension = this.GetFileExtension(fileName);
+            return $"{guid}_{typeName}.{fileExtension}";
+        }
 
-		public String GetFileExtension(String fileName) {
-			var splittedByPoint = fileName.Split('.');
-			return splittedByPoint[splittedByPoint.Length - 1];
-		}
-	}
+        public String GetFileExtension(String fileName)
+        {
+            var splittedByPoint = fileName.Split('.');
+            return splittedByPoint[splittedByPoint.Length - 1];
+        }
+    }
 }
